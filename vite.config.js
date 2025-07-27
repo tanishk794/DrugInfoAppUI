a9b0dac11b33cb965d323fc84788d6cdc9c99f9f
Tanishk Srivastava <tanishk794@gmail.com>
2025-07-28 03:13:37 +0530

    fifth

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // allows external access
+    port: process.env.PORT || 4173, // lets Render bind to the correct port
+    allowedHosts: ['druginfoappui-1.onrender.com'], // your Render domain
+  },
 })
